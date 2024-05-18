052c83cd9e5b0f383853767c78aeb983ba41dc19
Zach 🦀🌲 <zachary.jay.crabtree@gmail.com>
2024-05-18 14:44:02 -0700

    Add comic intro

--- main_menu.gd
+++ main_menu.gd
@@ -10,6 +10,59 @@ func _on_play_button_mouse_exited():
     playButton.modulate = Color("#ffffff")
 
 func _on_play_button_pressed():
+    $UI.visible = false
+    $Comic.visible = true
+
+    # Hide all comic elements other than Panel1 background
+    $Comic/Panel1/Background.modulate = Color("ffffff")
+    $Comic/Panel1/Text.modulate = Color("ffffff00")
+    $Comic/Panel2/Background.modulate = Color("ffffff00")
+    $Comic/Panel2/Text.modulate = Color("ffffff00")
+    $Comic/Panel2/Text2.modulate = Color("ffffff00")
+    $Comic/Panel3/Background.modulate = Color("ffffff00")
+    $Comic/Panel3/Text.modulate = Color("ffffff00")
+    $Comic/Panel3/Text2.modulate = Color("ffffff00")
+    $Comic/Panel3/DRRR1.modulate = Color("ffffff00")
+    $Comic/Panel3/DRRR2.modulate = Color("ffffff00")
+    $Comic/Panel3/Eye.modulate = Color("ffffff00")
+
+    var tween = get_tree().create_tween()
+    # Animate panel 1
+    tween.tween_property($Comic/Panel1/Text, "modulate", Color("ffffff"), 2)
+
+    # Transition panel 1 -> 2
+    tween.tween_property($Comic/Panel1/Background, "modulate", Color("ffffff00"), 1).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel1/Text, "modulate", Color("ffffff00"), 1).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel2/Background, "modulate", Color("ffffff"), 1).set_delay(5)
+
+    #Animate panel 2
+    tween.tween_property($Comic/Panel2/Text, "modulate", Color("ffffff"), 2)
+    tween.tween_property($Comic/Panel2/Text2, "modulate", Color("ffffff"), 2)
+
+    # Transition panel 2 -> 3
+    tween.tween_property($Comic/Panel2/Text, "modulate", Color("ffffff00"), 1).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel2/Text2, "modulate", Color("ffffff00"), 1).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel2/Background, "modulate", Color("ffffff00"), 1).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel3/Background, "modulate", Color("ffffff"), 1).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel3/Eye, "modulate", Color("ffffff"), 1).set_delay(5)
+
+    # Animate panel 3
+    tween.tween_property($Comic/Panel3/Text, "modulate", Color("ffffff"), 2)
+    tween.tween_property($Comic/Panel3/DRRR1, "modulate", Color("ffffff"), 2)
+    tween.parallel().tween_property($Comic/Panel3/DRRR2, "modulate", Color("ffffff"), 2).set_delay(.5)
+    tween.tween_property($Comic/Panel3/Text2, "modulate", Color("ffffff"), 2).set_delay(.5)
+
+    # Transition panel 3 out
+    tween.tween_property($Comic/Panel3/Background, "modulate", Color("ffffff00"), 2).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel3/Text, "modulate", Color("ffffff00"), 2).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel3/Text2, "modulate", Color("ffffff00"), 2).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel3/DRRR1, "modulate", Color("ffffff00"), 2).set_delay(5)
+    tween.parallel().tween_property($Comic/Panel3/DRRR2, "modulate", Color("ffffff00"), 2).set_delay(5)
+
+    tween.tween_property($Comic/Panel3/Eye, "modulate", Color("ffffff00"), .5).set_delay(2)
+    tween.tween_callback(start_game)
+
+func start_game():
     var game = preload("res://game.tscn").instantiate()
     get_tree().root.add_child(game)
     get_tree().root.remove_child(self)
